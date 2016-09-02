382354aabf4197f6978690aaae8e18662aec2d16
daulet <daucloudlab@gmail.com>
2016-09-02 12:54:36 +0600

    Maven плагинің пайдалана отырып орындалатын jar файлды құру

---
+++ src/main/java/kz/kaznitu/Hello.java
@@ -0,0 +1,9 @@
+package kz.kaznitu;
+
+import javax.swing.*;
+
+public class Hello {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World!");
+    }
+}
